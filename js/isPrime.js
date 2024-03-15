8d87d6bd71019364241a05b7b7444e466db4577e
耀 <kai@yaodebijixingdiannao.local>
2024-03-15 16:48:08 +0800

    add isPrime.js

---
+++ js/isPrime.js
@@ -0,0 +1,15 @@
+function isPrime(n){
+    for(let i = 2;i < n;i++){
+        if(n%i==0)return false
+    }
+    return true
+}
+console.log('isPrime(5)=',isPrime(5))
+console.log('isPrime(6)=',isPrime(6))
+
+function printisPrime(n){
+    for(let i = 2;i<n;i++){
+        if(isPrime(i))console.log(i,'is a prime')
+    }
+}
+printisPrime(100)
